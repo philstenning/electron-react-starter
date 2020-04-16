656e068ed4897ab3528067faf22c3c172bfe81ec
philstenning <philstenning@hotmail.com>
2020-04-16 18:29:43 +0100

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const app = window.require("electron").remote;
+const path = app.require("path");
 
 function App() {
+  const dir = path.join(__dirname, "../build/index.html");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Electron-React-starter</h1>
+      <p>root path: {dir}</p>
     </div>
   );
 }
